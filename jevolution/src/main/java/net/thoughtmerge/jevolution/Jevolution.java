25f3abc67a4e60b0560af49cc501773336e22b79
evangates <evan@thoughtmerge.net>
2014-05-25 17:26:38 -0700

    draw a quad in update loop

--- jevolution/src/main/java/net/thoughtmerge/jevolution/Jevolution.java
+++ jevolution/src/main/java/net/thoughtmerge/jevolution/Jevolution.java
@@ -8,11 +8,13 @@
 
 package net.thoughtmerge.jevolution;
 
+import java.awt.Color;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +25,11 @@ import org.slf4j.LoggerFactory;
 public class Jevolution {
 
   private final static Logger logger = LoggerFactory.getLogger(Jevolution.class);
+
   public final static int DEFAULT_WIDTH = 1024;
   public final static int DEFAULT_HEIGHT = (int)((double)DEFAULT_WIDTH * 9.0 / 16.0);
   public final static String DEFAULT_TITLE = "Jevolution";
+  public final static Color QUAD_COLOR = Color.WHITE;
 
   public static void main(String[] args) {
     Jevolution game = null;
@@ -72,6 +76,8 @@ public class Jevolution {
 
       handleInput();
 
+      render();
+
       Display.update();
     }
   }
@@ -117,6 +123,33 @@ public class Jevolution {
     logger.info("{} was {}", key, wasPressed ? "pressed" : "released");
   }
 
-  private void initGL() {}
+  private void initGL() {
+    GL11.glMatrixMode(GL11.GL_PROJECTION);
+    GL11.glLoadIdentity();
+    GL11.glOrtho(0, DEFAULT_WIDTH, 0, DEFAULT_HEIGHT, 1, -1);
+    GL11.glMatrixMode(GL11.GL_MODELVIEW);
+  }
+
+  private void render() {
+    // clear the screen and depth buffer
+    GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+    // set the color of the quad (R, G, B, A)
+    GL11.glColor3f(QUAD_COLOR.getRed()/255.0f, QUAD_COLOR.getGreen()/255.0f, QUAD_COLOR.getBlue()/255.0f);
+
+    int llx = 0;
+    int lly = 0;
+    int width = DEFAULT_WIDTH/2;
+    int height = DEFAULT_HEIGHT/2;
+
+    // draw the quad
+    GL11.glBegin(GL11.GL_QUADS);
+      GL11.glVertex2f(llx, lly);
+      GL11.glVertex2f(llx+width, lly);
+      GL11.glVertex2f(llx+width, lly+height);
+      GL11.glVertex2f(llx, lly+height);
+    GL11.glEnd();
+  }
+
   private void resizeGL() {}
 }
